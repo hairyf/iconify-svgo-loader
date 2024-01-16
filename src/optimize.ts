86661dde9ce0056d8cfd43159b837e6638edd287
Mr.Mao <951416545@qq.com>
2024-01-16 23:19:19 +0800

    fix: optimize extract color length

--- src/optimize.ts
+++ src/optimize.ts
@@ -10,7 +10,15 @@ export function optimize(svg: string, options: TransformConfig = {}) {
   let { config = {}, prefix } = options
   config = { plugins: defaultPlugins, multipass: true, ...config }
 
-  const single = patch('fill', svg) + patch('stop-color', svg) === 1
+  const colors = new Set<string>([
+    ...extract(svg, 'fill'),
+    ...extract(svg, 'stop-color'),
+    ...extract(svg, 'stroke'),
+  ])
+  
+  colors.delete('none')
+
+  const single = colors.size <= 1
 
   if (prefix)
     config.plugins?.push({ name: 'prefixIds', params: { prefix } })
@@ -21,13 +29,18 @@ export function optimize(svg: string, options: TransformConfig = {}) {
   return _optimize(svg, config).data
 }
 
-export function patch(re: string, s: string) {
-  return s.split(re).length - 1
-}
 
 export function defaultTransform(name: string, svg: string, _config: Config = {}) {
   const config = { plugins: defaultPlugins, multipass: true, ..._config }
-  const single = patch('fill=', svg) + patch('stop-color=', svg) === 1
+  const colors = new Set<string>([
+    ...extract(svg, 'fill'),
+    ...extract(svg, 'stop-color'),
+    ...extract(svg, 'stroke'),
+  ])
+  
+  colors.delete('none')
+
+  const single = colors.size <= 1
 
   config.plugins?.push({ name: 'prefixIds', params: { prefix: name } })
 
@@ -35,4 +48,11 @@ export function defaultTransform(name: string, svg: string, _config: Config = {}
     config.plugins?.push({ name: 'convertColors', params: { currentColor: true } })
 
   return _optimize(svg, config).data
+}
+
+
+export function extract(html: string, key: string) {
+  var regex = new RegExp(key + '="([^"]+)"', "g");
+  var matches = html.matchAll(regex);
+  return [...matches].map(m => m[1])
 }
